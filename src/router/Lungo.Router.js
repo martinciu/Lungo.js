78ae454c0ca5df528c38c398d9755a15702e0b56
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2013-02-18 16:18:15 +0100

    remove class_hide

--- src/router/Lungo.Router.js
+++ src/router/Lungo.Router.js
@@ -39,7 +39,7 @@ Lungo.Router = (function(lng, undefined) {
                 }
 
                 current.removeClass(CLASS.SHOW).addClass(CLASS.HIDE);
-                target.addClass(CLASS.SHOW);
+                target.addClass(CLASS.SHOW).removeClass(CLASS.HIDE);
                 lng.Element.Cache.section = target;
                 lng.Element.Cache.article = target.find(ELEMENT.ARTICLE + '.' + CLASS.CURRENT);
 
